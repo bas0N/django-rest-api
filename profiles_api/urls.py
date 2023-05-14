fae493328b088276ee3bd2cc8d50672d230deb19
basON <wch.basinski@gmail.com>
2023-05-15 00:09:06 +0200

    Add seearch api

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -5,10 +5,10 @@ from profiles_api import views
 
 router=DefaultRouter()
 router.register('hello-viewset',views.HelloViewSets,base_name='helo-viewset')
-
+router.register('profile',views.UserProfileViewSet)
 
 urlpatterns = [
     path("hello-view/",views.HelloApiView.as_view()),
-    path('',include(router.urls))
+    path('',include(router.urls)),
 
 ]
